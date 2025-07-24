2e8a2082220d1a9e239939146d0d47020e1fe3e0
Arman7171 <armvar774@gmail.com>
2025-07-24 14:40:18 +0400

    base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black h-screen"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+const router = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default router;
